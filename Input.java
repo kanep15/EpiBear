17c552ed165140e32c499235d881adf58558c5ab
Alex <a.king.pro03@gmail.com>
2022-02-10 19:21:47 +0000

    First Steps

--- Input.java
+++ Input.java
@@ -1,20 +1,20 @@
 
-import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.Scanner; // Import the Scanner class to read text files
+import java.io.FileReader;
+import java.io.BufferedReader;
 
 public class Input {
     public static void main(String[] args){
-        try{
-            File input_file = new File("b_should_be_easy.txt");
-            Scanner myScanner = new Scanner(input_file);
-            while (myScanner.hasNextLine()) {
-                String data = myScanner.nextLine();
-                //Fuck knows what to do next, probably want an array or list not a string
-            }
+        Data();
+    }
+    public static BufferedReader Data() {
+        BufferedReader data = null;
+        try {
+            data = new BufferedReader(new FileReader("b_should_be_easy.txt"));
         } catch (FileNotFoundException e) {
             System.out.println("File Not Found");
             e.printStackTrace();
         }
+        return data;
     }
 }
