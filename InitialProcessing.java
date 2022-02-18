7dd175877eb707a6e3e5b0178556b681a35a8f5e
kanep15 <34273244+kanep15@users.noreply.github.com>
2022-02-18 18:19:20 +0000

    array list now INTEGERS

--- InitialProcessing.java
+++ InitialProcessing.java
@@ -13,8 +13,8 @@ public class InitialProcessing {
 
         //Uncomment the file your working with
 
-        String fileName = "a_example.txt";
-        //String fileName = "b_should_be_easy.txt";
+        //String fileName = "b_should_be_easy";
+        String fileName = "b_should_be_easy.txt";
 
         File data = new File(fileName);
         return filter(data);
@@ -22,10 +22,11 @@ public class InitialProcessing {
 
     private static ArrayList filter(File file) {
         ArrayList filteredArray = new ArrayList();
+        ArrayList<Integer[]> intArray = new ArrayList<Integer[]>();
 
         try {
             Scanner scanner = new Scanner(file);
-            filteredArray.add(scanner.nextLine()); //Always add the first line
+            //filteredArray.add(scanner.nextLine());//Always add the first line
 
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
@@ -37,6 +38,7 @@ public class InitialProcessing {
                 int finishColumn = 0;
                 int earliestStart = 0;
                 int latestFinish = 0;
+                Integer[] bucket = new Integer[0];
 
                 try {
                     startRow = Integer.parseInt(splitLine[0]);
@@ -45,6 +47,9 @@ public class InitialProcessing {
                     finishColumn = Integer.parseInt(splitLine[3]);
                     earliestStart = Integer.parseInt(splitLine[4]);
                     latestFinish = Integer.parseInt(splitLine[5]);
+
+
+
 
                 } catch (NumberFormatException e) {
                     System.out.println("Int conversion");
@@ -57,7 +62,8 @@ public class InitialProcessing {
                 if (timeRequired > timeGiven) {
                     //do nothing
                 } else {
-                    filteredArray.add(line);
+                    //filteredArray.add(line);
+                    intArray.add(new Integer[]{startRow,startColumn,finishRow,finishColumn,earliestStart,latestFinish});
                 }
             }
 
@@ -66,6 +72,7 @@ public class InitialProcessing {
             System.out.println("File Not Found");
             e.printStackTrace();
         }
-        return filteredArray;
+        //return filteredArray;
+        return intArray;
     }
 }
--- Main.java
+++ Main.java
@@ -4,42 +4,56 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args){
         ArrayList data = InitialProcessing.main();
+        ArrayList<Car> fleet = new ArrayList<Car>();
+        ArrayList<Integer[]> rides = new ArrayList<Integer[]>();
 
-        String firstRow = (String) data.get(0);
-        String[] processStats = firstRow.split(" ");
+        Integer[] firstRow = (Integer[]) data.get(0);
 
-        int rowNumber = Integer.parseInt(processStats[0]);
-        int columnNumber = Integer.parseInt(processStats[1]);
-        int fleetSize = Integer.parseInt(processStats[2]);
-        int numRides = Integer.parseInt(processStats[3]);
-        int bonus = Integer.parseInt(processStats[4]);
-        int time = Integer.parseInt(processStats[5]);
+
+        int rowNumber = firstRow[0];
+        int columnNumber = firstRow[1];
+        int fleetSize = firstRow[2];
+        int numRides = firstRow[3];
+        int bonus = firstRow[4];
+        int time =firstRow[5];
+
+        System.out.println(firstRow[0]);
 
         for (int i = 0; i < fleetSize; i++) {   //Set up a fleet of cars
             Car car = new Car(i);
+            fleet.add(car);
         }
 
         for (int i = 1; i < data.size(); i++) { //i = 1 to skip the first row.
             int currentTime = i - 1;    //I don't think this is the best way to keep time
-            String currentData = (String) data.get(i);
-            String[] processData = currentData.split(" ");
 
-            int startRow = Integer.parseInt(processData[0]);
-            int startColumn = Integer.parseInt(processData[1]);
-            int finishRow = Integer.parseInt(processData[2]);
-            int finishColumn = Integer.parseInt(processData[3]);
-            int start = Integer.parseInt(processData[4]);
-            int finish = Integer.parseInt(processData[5]);
+            Integer[] processData = (Integer[]) data.get(i);
+            int startRow = processData[0];
+            int startColumn = processData[1];
+            int finishRow = processData[2];
+            int finishColumn = processData[3];
+            int earliestStart = processData[4];
+            int latestFinish = processData[5];
+            int currentRow = startRow;
+            int currentColumn = startColumn;
 
             int timeRequired = timeRequ(startRow, finishRow, startColumn, finishColumn);
 
+            //need to make an array list with [rideNumber,startrow,startcol.currentrow,currentcol,endrow,endcol,earliestStart,latestFinish,timerequired]
+            rides.add(new Integer[]{i,startRow,startColumn,currentRow,currentColumn,finishRow,finishColumn,earliestStart,latestFinish,timeRequired,});
+
             for (i = 0; i < fleetSize; i++) {
 
+               //fleet.get(i).setCurrentRow(5);
+               //System.out.println(fleet.get(i));
             }
         }
     }
     private static int timeRequ(int startRow, int finishRow, int startColumn, int finishColumn) {
         return (finishRow - startRow) + (finishColumn - startColumn);
+
     }
+
+
 }
 
